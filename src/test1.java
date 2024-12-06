fbe8851e5b6ae8ddf4c340b51370ff6aa6681362
sterbens547 <miller5094328@gmail.com>
2024-12-06 16:04:16 +0300

    first

---
+++ src/test1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Ввод данных
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Ведиде число а:");
+        float num1 = scan.nextFloat();
+
+        System.out.println("введите число б");
+        float num2 = scan.nextFloat();
+        // сравнение
+        if (num1 > num2) {
+            System.out.println("Число а больше.");
+        }
+        else if (num1 < num2) {
+            System.out.println("Число б больше.");
+        } else if (num1 == num2) {
+            System.out.println("Числа равны");
+        }
+        //калякулятор
+        System.out.println("Сумма:" +(num1 + num2));
+        System.out.println("Вычетание:" +(num1 - num2));
+        System.out.println("Умножение:" + (num1 * num2));
+        if (num2 != 0) {
+            System.out.println("Деление:" + (num1 / num2));
+        }else
+            System.out.println("Действие недопустимо");
+
+
+
+    }
+    }
